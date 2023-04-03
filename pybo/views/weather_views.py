63c3aa3879276cf311a8fc235decb730d60e90fd
choijinhyuck <choijinhyuck@naver.com>
2023-04-03 09:00:01 +0900

    get a weather_forcast module

--- pybo/views/weather_views.py
+++ pybo/views/weather_views.py
@@ -1,10 +1,13 @@
 from flask import Blueprint, render_template
-from ...Weather_project import main as forecast
+import sys, os
 
 bp = Blueprint("weather", __name__, url_prefix="/weather")
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(__file__)))))
+from Weather_project.main import GetWeather
 
 
 @bp.route("/")
 def index():
-    info = "신성동 날씨는"
+    info = GetWeather("서울특별시", "관악구", "대학동")
     return render_template("weather/weather.html", info=info)
