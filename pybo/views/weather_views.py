adca676c85844c1e30122c34a6cf619bf87ff386
choijinhyuck <choijinhyuck@naver.com>
2023-04-19 14:13:42 +0900

    오류 체크용

--- pybo/views/weather_views.py
+++ pybo/views/weather_views.py
@@ -11,13 +11,10 @@ from Weather_project.main import GetWeather
 def index():
     site = request.args.to_dict()
     if "si" in site:
-        try:
-            info = GetWeather(site["si"], site["gun"], site["dong"])
+        info = GetWeather(site["si"], site["gun"], site["dong"])
             return render_template(
                 "weather/weather.html", info=info, site=[site["si"], site["gun"], site["dong"]]
             )
-        except Exception as e:
-            print(e)
             # return render_template(
             #     "weather/weather_index.html", msg="오류 발생. 현재 날씨 정보를 찾을 수가 없어요. 다시 선택해주세요."
         
